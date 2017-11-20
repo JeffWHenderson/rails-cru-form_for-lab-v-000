a60605a6aba7258ed96d47a05a4c4cd843221ca8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-20 19:44:41 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,21 +1,21 @@
 class GenresController < ApplicationController
   # def index
   # end
-  # 
+  #
   def show
     @genre = Genre.find(params[:id])
   end
-  
+
   def new
-    
+    @genre = Genre.new
   end
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
   def update
   end
 
@@ -24,5 +24,5 @@ class GenresController < ApplicationController
   def gerne_params(*args)
     params.require(:genre).permit(*args)
   end
-  
+
 end
