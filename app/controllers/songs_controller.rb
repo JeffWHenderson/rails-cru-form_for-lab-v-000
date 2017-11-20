552b20ab245e87c0fddecbae8536e01071675b6c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-20 20:01:53 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,6 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
-    #<%= (link_to @song.artist.name, artist_path(@song.artist)) if @song.artist %>
-    #<%= (link_to @song.genre.name, genre_path(@song.genre)) if @song.genre %>
   end
 
   def new
