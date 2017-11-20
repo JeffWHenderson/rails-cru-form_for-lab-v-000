6adc257979f913053bcf6dca3c906bb9e29bdcd1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-20 19:29:18 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,22 +1,22 @@
 class ArtistsController < ApplicationController
   # def index
   # end
-  
+
   def show
     @artist = Artist.find(params[:id])
   end
-  
+
   def new
     @artist = Artist.new
   end
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
 end
